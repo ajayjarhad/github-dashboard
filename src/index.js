e83e653fbd05b1ee408dcde098b24a6d52213dbd
Ajay Jarhad <ajayjarhad1@gmail.com>
2021-11-17 17:30:29 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//This is the root file, here we are configuring Apollo's graphql client and rendering App component.
+
+// React and Apollo's dependencies
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import "./index.css";
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://pr-dashboard.hem.staging.canonic.dev/graphql", //Replace this with your own cloned project's URI.
+  cache: new InMemoryCache(),
+});
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
